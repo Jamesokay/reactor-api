ef7cc141eac20446ec4956b0ae0dece063345828
James OKeeffe <jamesokeeffe50@gmail.com>
2022-02-25 01:08:48 +0800

    Hooking up MongoDB

--- routes/auth.js
+++ routes/auth.js
@@ -1,16 +1,27 @@
 const router = require('express').Router()
 const User = require('../models/User')
+const bcrypt = require('bcrypt')
 
 //Register
 router.post('/register', async (req, res) => {
-    const newUser = await new User({
-        username: req.body.username,
-        email: req.body.email,
-        password: req.body.password
-    })
+    try {
+        // Generate new password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(req.body.password, salt)
+        
+        // Create new user
+        const newUser = await new User({
+            username: req.body.username,
+            email: req.body.email,
+            password: hashedPassword
+        })
 
-    await user.save()
-    res.send('OKAY')
+        // Save user abd respond
+        const user = await newUser.save()
+        res.status(200).json(user)
+    } catch(err) {
+        console.error(err)
+    }
 })
 
 module.exports = router
