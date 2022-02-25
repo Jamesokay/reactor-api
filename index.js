1bf7590937e72cf745cf3119657761b052657dd1
James OKeeffe <jamesokeeffe50@gmail.com>
2022-02-26 00:16:57 +0800

    Posts routes added

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const helmet = require('helmet')
 const morgan = require('morgan')
 const userRoute = require('./routes/users')
 const authRoute = require('./routes/auth')
+const postRoute = require('./routes/posts')
 
 dotenv.config()
 
@@ -23,9 +24,11 @@ app.use(express.json())
 app.use(helmet())
 app.use(morgan('common'))
 
-app.use('/api/users', userRoute)
-
 app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
+app.use('/api/posts', postRoute)
+
+
 
 app.listen(5500, () => {
     console.log('Server listening')
